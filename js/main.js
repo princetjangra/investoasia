2f05adddee4390ace00a8d6bd46cce4aa2ef8663
prince <princetjangra@gmail.com>
2019-06-17 19:06:21 +0530

    Add platform page and coin1.html
    also fix scroll nav focus issue

--- js/main.js
+++ js/main.js
@@ -429,6 +429,118 @@ function openCity(cityName) {
 // comparison table end
 
 
+//  -------------------------------
+//       Platform Page start
+//  -------------------------------
+
+// Show coin on hovering over map ---------
+
+var india_hb = $(".india-hover-box");
+var singapore_hb = $(".singapore-hover-box");
+var indonesia_hb = $(".indonesia-hover-box");
+var hongkong_hb = $(".hong-kong-hover-box");
+var malaysia_hb = $(".malaysia-hover-box");
+var thailand_hb = $(".thailand-hover-box");
+var philippines_hb = $(".philippines-hover-box");
+var vietnam_hb = $(".vietnam-hover-box");
+var china_hb = $(".china-hover-box");
+var srilanka_hb = $(".sri-lanka-hover-box");
+var korea_hb = $(".korea-hover-box");
+var pak_hb = $(".pak-hover-box");
+
+var indiaSvg = $("#india");
+var singaporeSvg = $("#singapore");
+var indonesiaSvg = $("#indonesia");
+var hongkongSvg = $("#hong-kong");
+var malaysiaSvg = $("#malaysia");
+var thailandSvg = $("#thailand");
+var philippinesSvg = $("#philippines");
+var vietnamSvg = $("#vietnam");
+var chinaSvg = $("#china");
+var srilankaSvg = $("#sri-lanka");
+var koreaSvg = $("#korea");
+var pakSvg = $("#pak");
+
+indiaSvg.hover(() => india_hb.toggleClass("map-hover"));
+india_hb.hover(() => {
+  india_hb.toggleClass("map-hover");
+  indiaSvg.children().toggleClass("svg-map-hover");
+});
+
+singaporeSvg.hover(() => singapore_hb.toggleClass("map-hover"));
+singapore_hb.hover(() => {
+  singapore_hb.toggleClass("map-hover");
+  singaporeSvg.children().toggleClass("svg-map-hover");
+});
+
+indonesiaSvg.hover(() => indonesia_hb.toggleClass("map-hover"));
+indonesia_hb.hover(() => {
+  indonesia_hb.toggleClass("map-hover");
+  indonesiaSvg.children().toggleClass("svg-map-hover");
+});
+
+hongkongSvg.hover(() => hongkong_hb.toggleClass("map-hover"));
+hongkong_hb.hover(() => {
+  hongkong_hb.toggleClass("map-hover");
+  hongkongSvg.children().toggleClass("svg-map-hover");
+});
+
+malaysiaSvg.hover(() => malaysia_hb.toggleClass("map-hover"));
+malaysia_hb.hover(() => {
+  malaysia_hb.toggleClass("map-hover");
+  malaysiaSvg.children().toggleClass("svg-map-hover");
+});
+
+thailandSvg.hover(() => thailand_hb.toggleClass("map-hover"));
+thailand_hb.hover(() => {
+  thailand_hb.toggleClass("map-hover");
+  thailandSvg.children().toggleClass("svg-map-hover");
+});
+
+philippinesSvg.hover(() => philippines_hb.toggleClass("map-hover"));
+philippines_hb.hover(() => {
+  philippines_hb.toggleClass("map-hover");
+  philippinesSvg.children().toggleClass("svg-map-hover");
+});
+
+vietnamSvg.hover(() => vietnam_hb.toggleClass("map-hover"));
+vietnam_hb.hover(() => {
+  vietnam_hb.toggleClass("map-hover");
+  vietnamSvg.children().toggleClass("svg-map-hover");
+});
+
+chinaSvg.hover(() => china_hb.toggleClass("map-hover"));
+china_hb.hover(() => {
+  china_hb.toggleClass("map-hover");
+  chinaSvg.children().toggleClass("svg-map-hover");
+});
+
+srilankaSvg.hover(() => srilanka_hb.toggleClass("map-hover"));
+srilanka_hb.hover(() => {
+  srilanka_hb.toggleClass("map-hover");
+  srilankaSvg.children().toggleClass("svg-map-hover");
+});
+
+koreaSvg.hover(() => korea_hb.toggleClass("map-hover"));
+korea_hb.hover(() => {
+  korea_hb.toggleClass("map-hover");
+  koreaSvg.children().toggleClass("svg-map-hover");
+});
+
+pakSvg.hover(() => pak_hb.toggleClass("map-hover"));
+pak_hb.hover(() => {
+  pak_hb.toggleClass("map-hover");
+  pakSvg.children().toggleClass("svg-map-hover");
+});
+
+
+//  -------------------------------
+//        Platform Page end
+//  -------------------------------
+
+
+
+
 //  --------------------------
 //     About Us Page Start
 //  --------------------------
